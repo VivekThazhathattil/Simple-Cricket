a59da303c4f2f4201dac20568b2dcd7ca8792caa
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2020-05-30 16:09:23 +0530

    add ingame animations

--- scripts/base_game.gd
+++ scripts/base_game.gd
@@ -18,6 +18,7 @@ var ball_count = 0
 var player_wickets_rem = 10
 var opp_wickets_rem = 10
 var max_wickets = 10
+var balls_per_over = 6
 
 var num_sides_batted_so_far = 0
 var _player_batting
@@ -36,12 +37,12 @@ func _ready():
 	$player.set_texture(hand_10)
 	$opponent.set_texture(hand_10)
 	$opponent.flip_h = true
+	get_node("/root/menu/bg_music").playing = false
 	
 func _set_toss_decision():
 	_player_batting = $coin_toss._player_batting
 	print("base_game:38: coin_toss._player_batting = " + str($coin_toss._player_batting))
 	print("base_game:39: _player_batting = " + str(_player_batting))
-	_update_labels()
 
 func _end_match():
 	yield(get_tree().create_timer(1),"timeout")
@@ -53,6 +54,7 @@ func _end_match():
 			$they_won.visible = true
 		else:
 			$drawn.visible = true
+	_update_labels()
 			
 func _final_action():
 	queue_free()
@@ -70,7 +72,7 @@ func _main_handler():
 	if _player_batting:
 		if _not_out:
 			player_score += $button_array.instantaneous_score
-			$pump_up.set_text("")
+			$PUMP/pump_up.set_text("")
 			_update_labels()
 			_check_if_won()
 		else:
@@ -78,18 +80,21 @@ func _main_handler():
 				if num_sides_batted_so_far == 1:
 					_switch_sides()
 				else:
-					_update_labels()
 					_match_over = true
+					_update_labels()
 					_end_match()
 			else:
 				player_wickets_rem -= 1
 				_update_labels()
-			$pump_up.set_text("OUT")
+			$PUMP/pump_up.set_text("OUT!")
+			$PUMP/pump_up/AnimationPlayer.play("to_red")
+			$special_event_sound.stream = load("res://audio/out_sound.wav")
+			$special_event_sound.play()
 			_not_out = true
 	else:
 		if _not_out:
 			opp_score += $button_array.opponent_move
-			$pump_up.set_text("")
+			$PUMP/pump_up.set_text("")
 			_update_labels()
 			_check_if_won()
 		else:
@@ -97,13 +102,16 @@ func _main_handler():
 				if num_sides_batted_so_far == 1:
 					_switch_sides()
 				else:
-					_update_labels()
 					_match_over = true
+					_update_labels()
 					_end_match()
 			else:
 				opp_wickets_rem -= 1
 				_update_labels()
-			$pump_up.set_text("OUT")
+			$PUMP/pump_up.set_text("OUT!")
+			$PUMP/pump_up/AnimationPlayer.play("to_red")
+			$special_event_sound.stream = load("res://audio/out_sound.wav")
+			$special_event_sound.play()
 			_not_out = true
 
 	if ball_count == 6*over_max:
@@ -129,35 +137,151 @@ func _switch_sides():
 	yield(get_tree().create_timer(1),"timeout")
 	$innings_over.visible = true
 	_reset_hands()
+	_clear_labels()
 
+func _clear_labels():
+	$PUMP/pump_up.set_text("Continue...")
+	$my_stats/HBoxContainer/your_stat.set_text("")
+	$my_stats/HBoxContainer/your_stat2.set_text("")
+	$opp_stats/HBoxContainer/your_stat.set_text("")
+	$opp_stats/HBoxContainer/your_stat2.set_text("")
+	$ColorRect/misc_stats.set_text("Need " + str(abs(player_score-opp_score)+1) + " runs from " + str(over_max*6 - ball_count) + " balls to win.")
+	
 func _update_labels():
 	if _player_batting:
 		if player_score == 0 and opp_score == 0:
-			$pump_up.set_text("Begin!!!")
+			$PUMP/pump_up.set_text("Begin!!!")
+			$PUMP/pump_up/AnimationPlayer.play("to_green")
+			$special_event_sound.stream = load("res://audio/six_sound.wav")
+			$special_event_sound.play()
 		elif $button_array.instantaneous_score == 1:
-			$pump_up.set_text(str($button_array.instantaneous_score) + " run")
+			$PUMP/pump_up.set_text(str($button_array.instantaneous_score) + " run")
+		elif $button_array.instantaneous_score == 4:
+			$PUMP/pump_up.set_text(str($button_array.instantaneous_score) + " runs!")
+			$PUMP/pump_up/AnimationPlayer.play("to_yellow")
+			$special_event_sound.stream = load("res://audio/four_sound.wav")
+			$special_event_sound.play()
+		elif $button_array.instantaneous_score == 6:
+			$PUMP/pump_up.set_text(str($button_array.instantaneous_score) + " runs!!!")
+			$PUMP/pump_up/AnimationPlayer.play("to_green")
+			$special_event_sound.stream = load("res://audio/six_sound.wav")
+			$special_event_sound.play()
 		else:
-			$pump_up.set_text(str($button_array.instantaneous_score) + " runs")
+			$PUMP/pump_up.set_text(str($button_array.instantaneous_score) + " runs")
 		$my_stats/HBoxContainer/your_stat.set_text("You\n(Batting)")
 		$opp_stats/HBoxContainer/your_stat.set_text("Opponent\n(Bowling)")
 		$my_stats/HBoxContainer/your_stat2.set_text("Score: " + str(player_score) + "-" + str(max_wickets - player_wickets_rem))
 		$opp_stats/HBoxContainer/your_stat2.set_text("Overs: " + str(ball_count/6) + "." + str(ball_count%6))
 	else:
 		if player_score == 0 and opp_score == 0:
-			$pump_up.set_text("Begin!!!")
+			$PUMP/pump_up.set_text("Begin!!!")
+			$PUMP/pump_up/AnimationPlayer.play("to_green")
+			$special_event_sound.stream = load("res://audio/six_sound.wav")
+			$special_event_sound.play()
+		elif $button_array.opponent_move == 4:
+			$PUMP/pump_up.set_text(str($button_array.opponent_move) + " runs!")
+			$PUMP/pump_up/AnimationPlayer.play("to_yellow")
+			$special_event_sound.stream = load("res://audio/four_sound.wav")
+			$special_event_sound.play()
+		elif $button_array.opponent_move == 6:
+			$PUMP/pump_up.set_text(str($button_array.opponent_move) + " runs!!!")
+			$PUMP/pump_up/AnimationPlayer.play("to_green")
+			$special_event_sound.stream = load("res://audio/six_sound.wav")
+			$special_event_sound.play()
 		elif $button_array.opponent_move == 1:
-			$pump_up.set_text(str($button_array.opponent_move) + " run")
+			$PUMP/pump_up.set_text(str($button_array.opponent_move) + " run")
 		else:
-			$pump_up.set_text(str($button_array.opponent_move) + " runs")
+			$PUMP/pump_up.set_text(str($button_array.opponent_move) + " runs")
 		$my_stats/HBoxContainer/your_stat.set_text("You\n(Bowling)")
 		$opp_stats/HBoxContainer/your_stat.set_text("Opponent\n(Batting)")
 		$my_stats/HBoxContainer/your_stat2.set_text("Overs: " + str(ball_count/6) + "." + str(ball_count%6))
 		$opp_stats/HBoxContainer/your_stat2.set_text("Score: " + str(opp_score) + "-" + str(max_wickets - opp_wickets_rem))
-
-
+	if num_sides_batted_so_far == 1:
+		if _player_batting:
+			if ball_count == 0:
+				$ColorRect/misc_stats.set_text("Current runrate: -")
+			else:
+				print(str(ball_count/balls_per_over + 0.1*(ball_count%balls_per_over)))
+				$ColorRect/misc_stats.set_text("Current runrate: " + str(stepify(float(player_score)/ball_count*balls_per_over,0.01)))
+		else:
+			if ball_count == 0:
+				$ColorRect/misc_stats.set_text("Current runrate: -")
+			else:
+				print(str(ball_count/balls_per_over + 0.1*(ball_count%balls_per_over)))
+				$ColorRect/misc_stats.set_text("Current runrate: " + str(stepify(float(opp_score)/ball_count*balls_per_over,0.01)))
+	elif num_sides_batted_so_far == 2:
+		if randi()%2 == 0:
+			if not _match_over:
+				$ColorRect/misc_stats.set_text("Need " + str(abs(player_score-opp_score)+1) + " runs from " + str(over_max*6 - ball_count) + " balls to win.")
+			else:
+				$ColorRect/misc_stats.set_text("Match finished!")
+		else:
+			if not _match_over:
+				if _player_batting:
+					if ball_count == 0 or ball_count == over_max*balls_per_over:
+						$ColorRect/misc_stats.set_text("Current runrate: -")
+					else:
+						$ColorRect/misc_stats.set_text("Current runrate: " + str(stepify(float(player_score)/ball_count*balls_per_over,0.01)) + "\n required runrate: " + str(stepify(float(opp_score-player_score)/(balls_per_over*over_max-ball_count)*balls_per_over,0.01)))
+				else:
+					if ball_count == 0 or ball_count == over_max*balls_per_over:
+						$ColorRect/misc_stats.set_text("Current runrate: -")
+					else:
+						$ColorRect/misc_stats.set_text("Current runrate: " + str(stepify(float(opp_score)/ball_count*balls_per_over,0.01)) + "\n required runrate: " + str(stepify(float(player_score-opp_score)/(balls_per_over*over_max-ball_count)*balls_per_over,0.01)))
+			else:
+				$ColorRect/misc_stats.set_text("Match finished!")
 func _on_continue_pressed():
 	_final_action()
 	get_tree().change_scene("res://scenes/menu.tscn")
 
 func _on_continue2_pressed():
 	$innings_over.visible = false
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	$PUMP/pump_up.set("custom_colors/font_color", Color(1,1,1,1))
+	$PUMP/pump_up.set("custom_fonts/font:size", str(50))
+	print("animatiom finished")
+
+
+func _on_Button_pressed():
+	print("button pressed")
+	if not _match_over and num_sides_batted_so_far == 2:
+		print("match not over")
+		if not "Current" in $ColorRect/misc_stats.get_text():
+			print("current in misc_stats")
+			if _player_batting:
+				print("player batting")
+				if ball_count == 0 or ball_count == over_max*balls_per_over:
+					$ColorRect/misc_stats.set_text("Current runrate: -")
+				else:
+					print("printing current run rate")
+					$ColorRect/misc_stats.set_text("Current runrate: " + str(stepify(float(player_score)/ball_count*balls_per_over,0.01)) + "\n required runrate: " + str(stepify(float(opp_score-player_score)/(balls_per_over*over_max-ball_count)*balls_per_over,0.01)))
+			else:
+				if ball_count == 0 or ball_count == over_max*balls_per_over:
+					$ColorRect/misc_stats.set_text("Current runrate: -")
+				else:
+					$ColorRect/misc_stats.set_text("Current runrate: " + str(stepify(float(opp_score)/ball_count*balls_per_over,0.01)) + "\n required runrate: " + str(stepify(float(player_score-opp_score)/(balls_per_over*over_max-ball_count)*balls_per_over,0.01)))
+		else:
+			print("current not in misc_stats")
+			$ColorRect/misc_stats.set_text("Need " + str(abs(player_score-opp_score)+1) + " runs from " + str(over_max*6 - ball_count) + " balls to win.")
+	elif not _match_over and num_sides_batted_so_far == 1:
+		print("match not over and num_sides is 1")
+		if not "Current" in $ColorRect/misc_stats.get_text():
+					print("current found in misc_stats")
+					if _player_batting:
+						if ball_count == 0:
+							$ColorRect/misc_stats.set_text("Current runrate: -")
+						else:
+							print(str(ball_count/balls_per_over + 0.1*(ball_count%balls_per_over)))
+							$ColorRect/misc_stats.set_text("Current runrate: " + str(stepify(float(player_score)/ball_count*balls_per_over,0.01)))
+					else:
+						if ball_count == 0:
+							$ColorRect/misc_stats.set_text("Current runrate: -")
+						else:
+							print(str(ball_count/balls_per_over + 0.1*(ball_count%balls_per_over)))
+							$ColorRect/misc_stats.set_text("Current runrate: " + str(stepify(float(opp_score)/ball_count*balls_per_over,0.01)))
+		else:
+			print("current not in misc_stats")
+			$ColorRect/misc_stats.set_text("Odd or Even Hand Cricket")
+	else:
+		$ColorRect/misc_stats.set_text("Match finished!")
